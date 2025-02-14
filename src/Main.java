a3df2792f95b873a21e3732d5657b186dff3370f
Bruno M Moreira <bruno_moreira.19@outlook.com>
2025-02-13 23:29:28 -0300

    :tada: exercicio de java pra deixar de ser burro, mas ainda me sinto burro, muito burro

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,7 @@ public class Main {
     double valorTransacao;
     int opcao;
 
+    System.out.println("---------------------------MOREIRA-BANK---------------------------");
     System.out.println("Digite 1-Depositar 2-Sacar 3-Extrato 4-Sair");
 
     do {
@@ -27,21 +28,24 @@ public class Main {
           System.out.println("Digite o valor do deposito: ");
           valorTransacao = input.nextDouble();
           conta.depositar(valorTransacao);
-          System.out.printf("Voce depositou R$ %.2f | Saldo atual R$ %.2f", valorTransacao, conta.saldo);
+          System.out.printf("Voce depositou R$ %.2f | Saldo atual R$ %.2f\n", valorTransacao, conta.saldo);
+          System.out.println("------------------------------------------------------");
           break;
         case 2:
-          System.out.println("Digite o valor do deposito: ");
+          System.out.println("Digite o valor do saque: ");
           valorTransacao = input.nextDouble();
           boolean sucesso =conta.sacar(valorTransacao);
           if(sucesso) {
-            System.out.printf("Voce sacou R$ %.2f | Saldo atual R$ %.2f", valorTransacao, conta.saldo);
+            System.out.printf("Voce sacou R$ %.2f | Saldo atual R$ %.2f\n", valorTransacao, conta.saldo);
           }else{
             System.out.println("Não é possível fazer o saque pois voce não tem saldo seu pobre!");
           }
+          System.out.println("------------------------------------------------------");
           break;
         case 3:
           String dadosConta = conta.exibirConta();
           System.out.println(dadosConta);
+          System.out.println("------------------------------------------------------");
           break;
         case 4:
           System.out.println("Valeu seu quebrado, até a proxima!");
@@ -50,7 +54,6 @@ public class Main {
         default:
           System.out.println("Opção inválida! Digite 1-Depositar 2-Sacar 3-Extrato 4-Sair");
       }
-
 
     }while (opcao != 4);
   }
